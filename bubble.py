8693e7bdf6ce0c42360499cd99532d429b83f66e
nestark <nestark@users.noreply.github.com>
2018-12-18 21:31:05 +0800

    add input method

--- bubble.py
+++ bubble.py
@@ -1,11 +1,18 @@
-a=[12,2,3,5,4,8,23,14,7,21]
-for n in range(9):
-    while n<9:
-        if a[n]>a[n+1]:
-            b=a[n+1]
-            a[n+1]=a[n]
-            a[n]=b
-            n+=1
-        else:
-            n+=1
-print(a)
+def bubblesort(a):
+    lenth = len(a)-1
+    for n in range(lenth):
+        while n<lenth:
+            if a[n] > a[n+1]:
+                b = a[n+1]
+                a[n+1] = a[n]
+                a[n] = b
+                n += 1
+            else:
+                n += 1
+    print(a)
+
+
+mes = input('Please input a list of number, divided by space:')
+nums = list(int(x) for x in mes.split())
+print(nums)
+bubblesort(nums)
