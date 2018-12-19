b575e91d3f6103f527e1601a9b77f198b48e7d13
nestark <nestark@users.noreply.github.com>
2018-12-19 21:55:02 +0800

    add comments

--- randname.py
+++ randname.py
@@ -22,7 +22,7 @@ class Application(Frame):
     def hello(self):
         """随机选取一个文本框中输入的值，显示在消息框中"""
         names = self.nameInput.get()#获取文本框输入内容
-        listed = list(x for x in names.split())#将输入的字符串以空格分割
+        listed = list(x for x in names.split(sep=' '))#将输入的字符串以空格分割, 如果以空格分割会报错, 需要修改
         self.choosed = self.randomselect(listed)
         name = self.choosed or 'You input nothing!'
         messagebox.showinfo(message=name)
@@ -36,4 +36,6 @@ class Application(Frame):
 if __name__ == "__main__":
     nameselect=Application()
     nameselect.master.title('Roll a Dice!')
+    nameselect.master.geometry('200x200')
+    nameselect.master.resizable(width=False,height=False)
     nameselect.mainloop()
