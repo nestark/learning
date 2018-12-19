b3688a624fec15a8128a5760e7b32ec202dd8cf5
nestark <nestark@users.noreply.github.com>
2018-12-19 21:38:42 +0800

    add comments

--- randname.py
+++ randname.py
@@ -10,23 +10,25 @@ class Application(Frame):
     def createWidgets(self):
             self.hellolabel=Label(self, text='Enter names',font=(20),width=20,height=2)
             self.hellolabel.grid()
-            self.nameInput = Entry(self, width=20)
+            self.nameInput = Entry(self, width=20)#创建文本输入框
             self.nameInput.grid()
 
     def roll(self):
             self.rollbutton = Button(self, text='Roll!', width=10, height=1,activeforeground='red',
                                      activebackground='blue',
-                                     command=self.hello)
+                                     command=self.hello)#创建按钮，设定大小和前景色背景色，点击运行hello函数
             self.rollbutton.grid(sticky='s')
 
     def hello(self):
-        names = self.nameInput.get()
-        listed = list(x for x in names.split(sep= '，'))
+        """随机选取一个文本框中输入的值，显示在消息框中"""
+        names = self.nameInput.get()#获取文本框输入内容
+        listed = list(x for x in names.split())#将输入的字符串以空格分割
         self.choosed = self.randomselect(listed)
         name = self.choosed or 'You input nothing!'
         messagebox.showinfo(message=name)
 
     def randomselect(self, nlist):
+        """输入一个列表，返回列表中的一个随机元素"""
         a = random.randint(0, len(nlist)-1)
         b = nlist[a]
         return b
@@ -35,4 +37,3 @@ if __name__ == "__main__":
     nameselect=Application()
     nameselect.master.title('Roll a Dice!')
     nameselect.mainloop()
-
